7b01ad399e3e01166748c27be254a7724d672061
Nghiatran2603 <107810710+NghiaTV2603@users.noreply.github.com>
2022-12-04 12:22:15 +0700

    init project Facebook

---
+++ src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+function Home() {
+    return (
+        <View style={{alignItems:'center',display:'flex',paddingTop:400}} >
+            <Text>Home!</Text>
+        </View>
+    )
+}
+
+export default Home
