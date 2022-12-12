980d212807c19e13f27e856f751d2f452b487b40
Nghiatran2603 <107810710+NghiaTV2603@users.noreply.github.com>
2022-12-12 23:22:25 +0700

    fix post

--- src/screens/NewFeed/components/AddPost.jsx
+++ src/screens/NewFeed/components/AddPost.jsx
@@ -1,40 +1,64 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View, TextInput } from "react-native";
 import { Avatar } from "@rneui/themed";
 import Entypo from "react-native-vector-icons/Entypo";
+import { BottomSheet, ListItem } from "@rneui/themed";
 
 export default function AddPost() {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 100,
-        alignItems: "center",
-        paddingLeft: 12,
-      }}
-    >
-      <Avatar
-        size={60}
-        rounded
-        source={{ uri: "https://randomuser.me/api/portraits/men/35.jpg" }}
-      />
+    <View>
       <View
         style={{
           flexDirection: "row",
-          backgroundColor: "#DCDCDC",
-          height: 36,
-          width: 280,
+          height: 100,
           alignItems: "center",
-          paddingLeft: 16,
-          marginLeft: 24,
-          borderRadius: 6,
+          paddingLeft: 12,
         }}
       >
-        <Text style={{ color: "#696969" }}>What's on your mind ? </Text>
-        <Entypo
-          name="images"
-          style={{ fontSize: 18, color: "grey", marginLeft: 86 }}
+        <Avatar
+          size={60}
+          rounded
+          source={{ uri: "https://randomuser.me/api/portraits/men/35.jpg" }}
         />
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            backgroundColor: "#DCDCDC",
+            height: 36,
+            width: 280,
+            paddingLeft: 16,
+            paddingRight: 16,
+            marginLeft: 24,
+            borderRadius: 6,
+          }}
+        >
+          <Text style={{ color: "#696969" }} onPress={() => setIsVisible(true)}>
+            What's on your mind ?{" "}
+          </Text>
+          <Entypo name="images" style={{ fontSize: 18, color: "grey" }} />
+        </View>
       </View>
+
+      <BottomSheet
+        onBackdropPress={() => {
+          setIsVisible(!isVisible);
+        }}
+        modalProps={{}}
+        isVisible={isVisible}
+      >
+        <ListItem>
+          <View style={{ height: 440 }}></View>
+        </ListItem>
+
+        <ListItem >
+          <View style={{flexDirection:'row',justifyContent:'center'}}>
+          </View>
+        </ListItem>
+      </BottomSheet>
     </View>
   );
 }
--- src/screens/components/Post.jsx
+++ src/screens/components/Post.jsx
@@ -26,7 +26,72 @@ export default function Post(post) {
   const [like, setLike] = React.useState(post.data.like);
   const [isLike, setIsLike] = React.useState(false);
   const [isVisible, setIsVisible] = React.useState(false);
-  const comments = [1,2,3,4,5,6,7,8,9,10]
+  const [comment, onChangeComment] = React.useState();
+  const [addComment, setAddComment] = React.useState(comments);
+  const handleAddComment = ()=>{
+    setAddComment()
+  }
+  console.log(comment)
+
+  
+  const newComment = {
+    avatar:post.data.Avatar,
+    comment:comment
+  }
+  const comments = [
+    {
+      avatar:
+        "https://image.tienphong.vn/600x315/Uploaded/2022/zaugtn/2022_05_10/avatar-609.jpg",
+      comment: "Nội dung có liên quan",
+    },
+    {
+      avatar:
+        "https://vcdn1-giaitri.vnecdn.net/2022/04/28/Avatar2JamesCameron-1651112439-5213-1651112580.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=hkDCL9zLv2Q3HB-EYkf6zQ",
+      comment: "Avatar 2 hé lộ những thước phim đầu tiên",
+    },
+    {
+      avatar:
+        "https://vcdn1-thethao.vnecdn.net/2022/11/30/Untitled-8697-1669804562.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=3Qi2v17m7Fy_udZMXg_RPg",
+      comment: "GOAT Messi",
+    },
+    {
+      avatar:
+        "https://cdn1.tuoitre.vn/zoom/600_315/2022/12/6/ronaldo-3-1670308112515303078038-crop-1670308130421159602441.jpg",
+      comment: "Cổ động viên Bồ Đào Nha không muốn Ronaldo",
+    },
+    {
+      avatar:
+        "https://image.thanhnien.vn/w1024/Uploaded/2022/oqivotiw/2022_09_23/messi-1482.jpeg",
+      comment:
+        "GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....",
+    },
+    {
+      avatar:
+        "https://image.thanhnien.vn/w1024/Uploaded/2022/oqivotiw/2022_09_23/messi-1482.jpeg",
+      comment:
+        "GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....GOAT Messi ....",
+    },
+    {
+      avatar:
+        "https://image.thanhnien.vn/w1024/Uploaded/2022/oqivotiw/2022_09_23/messi-1482.jpeg",
+      comment: "GOAT Messi ....GOAT Messi ....GOAT Messi ....",
+    },
+    {
+      avatar:
+        "https://image.thanhnien.vn/w1024/Uploaded/2022/oqivotiw/2022_09_23/messi-1482.jpeg",
+      comment: "GOAT Messi ....GOAT Messi ....GOAT Messi ....",
+    },
+    {
+      avatar:
+        "https://image.thanhnien.vn/w1024/Uploaded/2022/oqivotiw/2022_09_23/messi-1482.jpeg",
+      comment: "GOAT Messi ....GOAT Messi ....GOAT Messi ....",
+    },
+    {
+      avatar:
+        "https://image.thanhnien.vn/w1024/Uploaded/2022/oqivotiw/2022_09_23/messi-1482.jpeg",
+      comment: "GOAT Messi ....GOAT Messi ....GOAT Messi ....",
+    },
+  ];
 
   const handleLike = () => {
     if (isLike) {
@@ -136,48 +201,48 @@ export default function Post(post) {
         isVisible={isVisible}
       >
         <ListItem>
-          <View style={{ height: 400 }}>
-          <ScrollView style={{ width: "100%" }}>
-        {comments.map(()=>(
-            <View
-              style={{
-                flexDirection: "row",
-                width: 380,
-                alignItems: "center",
-                paddingLeft: 12,
-                paddingBottom: 12,
-
-              }}
-            >
-              <View style={{ flexDirection: "row"}}>
-                <Avatar size={40} rounded source={{ uri: post.data.avatar }} />
+          <View style={{ height: 440 }}>
+            <ScrollView style={{ width: "100%" }}>
+              {comments.map((cmt) => (
                 <View
                   style={{
-                    flexDirection: "column",
-                    marginLeft: 8,
-                    marginRight: 8,
-                    backgroundColor: "#D3D3D3",
-                    borderRadius: 8,
-                    padding: 8,
-
+                    flexDirection: "row",
+                    width: 380,
+                    alignItems: "center",
+                    paddingLeft: 12,
+                    paddingBottom: 12,
                   }}
                 >
-                  <Text style={styles.titleComment}>{post.data.user}</Text>
-                  <Text style={{marginRight:8}}> GOAT, Messi.... GOAT, Messi.... GOAT, Messi.... GOAT, Messi.... GOAT</Text>
+                  <View style={{ flexDirection: "row" }}>
+                    <Avatar size={40} rounded source={{ uri: cmt.avatar }} />
+                    <View
+                      style={{
+                        flexDirection: "column",
+                        marginLeft: 8,
+                        backgroundColor: "#D3D3D3",
+                        borderRadius: 8,
+                        padding: 8,
+                        width: 320,
+                      }}
+                    >
+                      <Text style={styles.titleComment}>{post.data.user}</Text>
+                      <Text style={{ marginRight: 8 }}>{cmt.comment}</Text>
+                    </View>
+                  </View>
                 </View>
-              </View>
-            </View>   
-        ))}
-        </ScrollView>
+              ))}
+            </ScrollView>
           </View>
         </ListItem>
 
         <ListItem>
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View
+            style={{ flexDirection: "row", alignItems: "center", height: 40 }}
+          >
             <TextInput
               style={{
                 height: 40,
-                margin: 12,
+                margin: 24,
                 borderWidth: 1,
                 width: 300,
                 borderRadius: 24,
@@ -185,11 +250,14 @@ export default function Post(post) {
                 paddingLeft: 16,
               }}
               placeholder="Write a comment..."
+              onChange={onChangeComment}
+              value={comment}
             />
             <MaterialCommunityIcons
               name="send"
               color="#1877F2"
               style={{ fontSize: 32 }}
+              onPress={handleAddComment}
             ></MaterialCommunityIcons>
           </View>
         </ListItem>
