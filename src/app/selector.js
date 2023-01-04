3412a59ea4fba85d28a34d28290ccc1871eb4adb
Nghiatran2603 <107810710+NghiaTV2603@users.noreply.github.com>
2023-01-05 01:18:49 +0700

    add chat

--- src/app/selector.js
+++ src/app/selector.js
@@ -2,3 +2,4 @@ import {createSelector} from "@reduxjs/toolkit";
 
 export const postNewFeedSelector = (state) => state.postNewFeed;
 export const commentPostSelector = (state) => state.post.comment;
+export const dataUserMessage = (state) => state.message;
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import newfeedSlice from "../screens/NewFeed/newfeedSlice";
 import postSlice from "../screens/components/postSlice";
+import messageSlice from "../screens/message/messageSlice";
 
 const store = configureStore({
     reducer:{
         postNewFeed: newfeedSlice.reducer,
         post: postSlice.reducer,
+        message: messageSlice.reducer,
     }
 })
 
