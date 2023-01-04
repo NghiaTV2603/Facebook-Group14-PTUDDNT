ae506ef3eeeafb1b1f31c6f7f753ee2977ef2886
Nghiatran2603 <107810710+NghiaTV2603@users.noreply.github.com>
2023-01-04 21:52:01 +0700

    fix

--- src/screens/NewFeed/NewFeed.jsx
+++ src/screens/NewFeed/NewFeed.jsx
@@ -1,35 +1,35 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-import { Divider } from "@rneui/base";
+import {Text, View} from "react-native";
+import {Divider} from "@rneui/base";
 import AddPost from "../newFeed/components/AddPost";
 import Post from "../components/Post";
-import { ScrollView } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
+import {ScrollView} from "react-native";
+import {useDispatch, useSelector} from "react-redux";
 import {postNewFeedSelector} from "../../app/selector";
 
 export default function NewFeed() {
-  const posts = useSelector(postNewFeedSelector);
-  return (
-    <ScrollView style={{ width: "100%" }}>
-      <View>
-        <AddPost />
-        <Divider
-          style={{ marginBottom: 16, backgroundColor: "#DCDCDC", height: 1 }}
-        />
-        {posts.map((post, index) => (
-          <View key={index}>
-            <Post dataPost={post} />
-            <Divider
-              style={{
-                marginBottom: 8,
-                marginTop: 8,
-                backgroundColor: "#DCDCDC",
-                height: 1,
-              }}
-            />
-          </View>
-        ))}
-      </View>
-    </ScrollView>
-  );
+    const posts = useSelector(postNewFeedSelector);
+    return (
+        <ScrollView style={{width: "100%"}}>
+            <View>
+                <AddPost/>
+                <Divider
+                    style={{marginBottom: 16, backgroundColor: "#DCDCDC", height: 1}}
+                />
+                {posts.map((post, index) => (
+                    <View key={index}>
+                        <Post dataPost={post}/>
+                        <Divider
+                            style={{
+                                marginBottom: 8,
+                                marginTop: 8,
+                                backgroundColor: "#DCDCDC",
+                                height: 1,
+                            }}
+                        />
+                    </View>
+                ))}
+            </View>
+        </ScrollView>
+    );
 }
